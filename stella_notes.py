2bfed051dfe4fd6369e9ba6c663b4c57580a45ae
stellasap <styliani_sapantzi@brown.edu>
2023-04-25 20:22:09 -0400

    Stella's notes on project plan
    Went to Ivan's office hours and he helped come up with a plan for the project and what steps to take next. Read about and started installing MMPose detector- 1 hour

---
+++ stella_notes.py
@@ -0,0 +1,18 @@
+#  https://mmpose.readthedocs.io/en/latest/overview.html
+#MMLab--MMPose package notes (Stella read documentation, introduction, installment instructions)
+# 2d hand pose estimation, consists of 8 main components that do important things for detection including datasets
+
+#Ivan Tips (office hours on 04/25/2023)
+#Stella's Tasks:
+# start woth installing and running MMPose detector
+#classifiers to recognize our pistures as their letters
+#Stella's and Camille's Tasks:
+# take 10 pictures of our hands for each letter (create our dataset)
+#Camille's tasks:
+#start working on the interface of the game (interactive aspects, presenting random letters of the english alphabet)
+
+#to do with Ivn in class:
+#help with classification and connecting pose detection to our dataset
+
+#final step:
+#connect game aspect made by Camille to detection aspect made by Stella= project :)
