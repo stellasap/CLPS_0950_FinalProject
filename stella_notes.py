27d85a18de84a3b1ec5cab0b92f20c5c1da60aab
stellasap <styliani_sapantzi@brown.edu>
2023-05-03 14:26:47 -0400

    Stella updates on dataset research and model

--- stella_notes.py
+++ stella_notes.py
@@ -104,3 +104,23 @@ test_dataloader = val_dataloader
 # the miniconda installed but maybe not in the right directory? it is in my computer but won't listen to python and install the rest of the things I need 
 # maybe need to uninstall and install again, have been stuck on that for the entire weekend
 #
+
+#05/03/2023
+#preparing my dataset and introducing it to MMPose:
+
+#1. dataset should be in a specific format, such as COCO or MPII. Each image should be annotated with the corresponding keypoints for the pose estimation task. 
+# use tools such as LabelImg or COCO Annotator to annotate your dataset.
+
+#2. Convert dataset to the MMPose format: MMPose requires the dataset to be in a specific format. 
+# use the tools/convert_datasets.py script in the MMPose repository to convert dataset.
+
+#3. Create a data loader: MMPose uses data loaders to load the dataset during training and evaluation. 
+# create a data loader for dataset by creating a new file in the mmpose/datasets directory. use the existing data loaders as a reference.
+
+#4. Register dataset with MMPose by adding it to the DATASET_REGISTRY dictionary in the mmpose/datasets/__init__.py file.
+
+#5. Train the model: Once dataset is registered, use the tools/train.py script. 
+# use the --config argument to specify the configuration file for the model. The configuration file should include the name of your dataset and the path to the data.
+
+#6. Evaluate the model: After training, evaluate the model using the tools/test.py script. 
+#  use the --config argument to specify the configuration file for your model.
