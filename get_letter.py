2820f0a4c606145f15dd4c77b618f25367fb80aa
camille-aquino <111753830+camille-aquino@users.noreply.github.com>
2023-05-11 18:30:02 -0400

    time: 5 hrs
    meeting w stella and ivan and trying to make this code merge *SUFFERING* *ivan should get a raise* + making demo and presentation

--- get_letter.py
+++ get_letter.py
@@ -1,5 +1,5 @@
 import random
 def get_letter(length):
-    letters = 'ABCDEFGHIKLMNOPQRSTUVWXYabcdefghikmnopqrstuvwxy'
+    letters = 'ABCDEFGHIKLMNOPQRSTUVWXY' # removed lowercase since Stella's only has upper case
     result_str = ''.join(random.choice(letters) for i in range(length))
     return result_str
